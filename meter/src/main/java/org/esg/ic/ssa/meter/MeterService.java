2f36c11c2d690836eb5449bde7950aa19205217f
Adrian Minde <adrian_minde@live.de>
2023-12-11 13:47:10 +0100

    Update gradle build files and license headers

--- meter/src/main/java/org/esg/ic/ssa/meter/MeterService.java
+++ meter/src/main/java/org/esg/ic/ssa/meter/MeterService.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2023-24 ISC Konstanz
+ *
+ * This file is part of OpenSSA.
+ * For more information visit https://github.com/isc-konstanz/OpenSSA.
+ *
+ * OpenSSA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenSSA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenSSA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.esg.ic.ssa.meter;
 
 import java.time.Instant;
@@ -17,26 +37,6 @@ import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 
-/*
- * Copyright 2023-24 ISC Konstanz
- *
- * This file is part of OpenSSA.
- * For more information visit https://github.com/isc-konstanz/OpenSSA.
- *
- * OpenSSA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OpenSSA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with OpenSSA. If not, see <http://www.gnu.org/licenses/>.
- *
- */
 public class MeterService extends ServiceAdapter {
 
 	static final String GRAPH_PATTERN = "meter.gp";
